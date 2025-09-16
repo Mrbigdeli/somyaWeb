cfc359e70eaf42271680b6e11ab32cb88b1e1cf6
Mr.bigdeli <bigdeli.webdevelop@gmail.com>
2025-09-16 18:28:40 +0330

    header improve3

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,11 +1,42 @@
 import { Link } from "react-router-dom";
 import "./Header.css";
+import { FiPhoneCall } from "react-icons/fi";
+import { LuMapPin, LuSearch, LuMail } from "react-icons/lu";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 const Header = () => {
   return (
     <div className="menuParent">
       <div id="MenuInfo">
-        <div id="MenuInfo-Left"></div>
+        <div id="MenuInfo-Left">
+          <div className="MenuInfo-Left-Items">
+            <FiPhoneCall
+              id="MenuInfo-Left-phoneIcon"
+              className="MenuInfo-Left-Icons"
+            />
+            <Link to="/" className="MenuInfo-Left-Items-link">
+              Call: +001 321 3456
+            </Link>
+          </div>
+          <div className="MenuInfo-Left-Items">
+            <LuMail
+              id="MenuInfo-Left-mailIcon"
+              className="MenuInfo-Left-Icons"
+            />
+            <Link to="/" className="MenuInfo-Left-Items-link">
+              Mail: support@norflo.com
+            </Link>
+          </div>
+          <div className="MenuInfo-Left-Items">
+            <LuMapPin
+              id="MenuInfo-Left-mailIcon"
+              className="MenuInfo-Left-Icons"
+            />
+            <Link to="/" className="MenuInfo-Left-Items-link">
+              Visit: 1485 Bayshore Blvd. Ste 154, San Francisco, CA 95124
+            </Link>
+          </div>
+        </div>
         <div id="MenuInfo-Right">
           <div id="MenuInfo-Right-MyAccount">
             <Link to="/" className="">
@@ -20,8 +51,40 @@ const Header = () => {
           </div>
         </div>
       </div>
-      <div id="MainMenu"></div>
-      <div id="DiscountMenu"></div>
+      <div id="MainMenu">
+        <div id="MainMenu-Section-Left"></div>
+        <div id="MainMenu-Section-Middle">
+          <div className="MainMenu-Section-middle-Item cursor-Pinter">Home</div>
+          <div className="MainMenu-Section-middle-Item cursor-Pinter">
+            Services
+          </div>
+          <div className="MainMenu-Section-middle-Item cursor-Pinter">
+            Pages
+          </div>
+          <div className="MainMenu-Section-middle-Item cursor-Pinter">Shop</div>
+          <div className="MainMenu-Section-middle-Item cursor-Pinter">Blog</div>
+          <div className="MainMenu-Section-middle-Item cursor-Pinter">
+            Pricing Plan
+          </div>
+          <div className="MainMenu-Section-middle-Item cursor-Pinter">
+            Contact Us
+          </div>
+        </div>
+        <div className="cursor-Pinter" id="MainMenu-Section-Right">
+          <AiOutlineShoppingCart />
+          <LuSearch />
+          <div className="cursor-Pinter" id="MainMenu-Section-Right-btn">
+            Get Started
+          </div>
+        </div>
+      </div>
+      <div id="DiscountMenu">
+        <span>
+          Save $15/mo on your internet bill. When you sign up for both mobile
+          and home internet.
+        </span>
+        <span>Offer details</span>
+      </div>
     </div>
   );
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import App from "./App.jsx";
 import Error from "./components/Error/Error";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./Layout";
+import "./assets/styles/generals.css";
 import "./assets/styles/global.css";
 import "./i18n";
 
